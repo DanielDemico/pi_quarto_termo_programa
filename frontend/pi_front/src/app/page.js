817a2b4006412b17cb16e1c4e1c6500bea14c0c8
Daniel Demico <ddemico3@gmail.com>
2025-11-17 23:31:56 -0300

    refactor: vite TO next

---
+++ frontend/pi_front/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1> Slc, zé pediu tá feito</h1>
+      <h2> Frontend da LuLu agora em Next</h2>
+    </div>
+  );
+}
